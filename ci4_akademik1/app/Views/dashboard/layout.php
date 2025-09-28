1c406baf6d7f30ab4c0ab49e853d74ae86b943a8
Nike Kustiane <nkustiane@gmail.com>
2025-09-28 12:56:39 +0700

    Delete ci4_akademik1/app directory

--- ci4_akademik1/app/Views/dashboard/layout.php
+++
@@ -1,6 +0,0 @@
-<?= $this->extend('layouts/main') ?>
-
-<?= $this->section('content') ?>
-    <h1>Dashboard</h1>
-    <p>Selamat datang, <?= session()->get('username') ?>!</p>
-<?= $this->endSection() ?>
